93249876796afcd0c3ef01b4bfdce761e2ba66e2
dboulytchev <dboulytchev@gmail.com>
2013-04-15 15:15:26 +0000

    Synched with transformers

--- src/Extensions.ml
+++ src/Extensions.ml
@@ -38,7 +38,7 @@ module Breaks =
 
         class ['self, 'e] interpret =
           object (this)
-            inherit ['self, 'e L.Expr.t, int, ('self * 'self * State.t), State.t] ttt
+            inherit ['self, 'e L.Expr.t', int, ('self * 'self * State.t), State.t] ttt
             method m_Skip (k, b, s) t = t.Generic.g (`Lambda, b, s) k
 
             method m_Assign env t x e = 
@@ -142,7 +142,7 @@ module Procedures =
 
         class ['self, 'e] code =
           object
-            inherit ['self, 'e L.Expr.t, string list, unit, string list] t_t
+            inherit ['self, 'e L.Expr.t', string list, unit, string list] t_t
             method m_Proc _ stmt name args locals body = 
               let sl l = string_of_int (length l) :: l in
               ["proc"; name] @ (sl args) @ (sl locals) @ (body.Generic.f ())
@@ -182,6 +182,8 @@ module Arrays =
         | `Elem  of 'self t * 'self t
         ]
 
+        type p = [ 'self L.Expr.t | 'self t | 'self Procedures.Expr.t] as 'self
+
         class ['self] code =
           object (self)
             inherit ['self, unit, string list] t_t
@@ -202,10 +204,8 @@ module Arrays =
           let tr = new gcode in 
           (L.Expr.t.Generic.gcata_ext tr ++ t.Generic.gcata_ext tr ++ Procedures.Expr.t.Generic.gcata_ext tr) apply () e
 
-         type p = [ 'self L.Expr.closed_t | 'self closed_t | 'self Procedures.Expr.closed_t] as 'self
-
       end
-
+   
     module Stmt =
       struct
 
--- src/L.ml
+++ src/L.ml
@@ -125,7 +125,7 @@ module Stmt =
 
     class ['self, 'e] interpret =
       object (this)
-        inherit ['self, 'e Expr.t, int, State.t, State.t] t_t         
+        inherit ['self, 'e Expr.t', int, State.t, State.t] t_t         
         method m_Skip s _ = s
         method m_Assign s _ x e = State.modify s x (e.Generic.f s)
         method m_Read s _ x = 
@@ -146,7 +146,7 @@ module Stmt =
 
     class ['self, 'e] print =
       object (this)
-        inherit ['self, 'e Expr.t, printer, unit, printer] t_t
+        inherit ['self, 'e Expr.t', printer, unit, printer] t_t
         method m_Skip   _ _       = string "skip"
         method m_Assign _ _ x e   = v [string x; string ":="; e.Generic.f ()]
         method m_If     _ _ c x y = v [string "if"; c.Generic.f (); v [string "then"; x.Generic.f (); string "else"; y.Generic.f ()]]
@@ -158,7 +158,7 @@ module Stmt =
 
     class ['self, 'e] code =
       object (this)
-        inherit ['self, 'e Expr.t, string list, unit, string list] t_t
+        inherit ['self, 'e Expr.t', string list, unit, string list] t_t
         method m_Skip   _ _       = ["s"]
         method m_Seq    _ _ x y   = ";" :: (x.Generic.f ()) @ (y.Generic.f ())
         method m_Assign _ _ x e   = "=" :: x :: (e.Generic.f ())
@@ -217,7 +217,7 @@ module Compiler =
 
         class ['self, 'e] compile =
           object (this)
-            inherit ['self, 'e Expr.t, string list, env, string list * int] t_t
+            inherit ['self, 'e Expr.t', string list, env, string list * int] t_t
             method m_Skip (this, next, last) _  = 
               (match this, next with 
                | `No   , `Maybe n            -> []
@@ -274,7 +274,7 @@ module Compiler =
 module Program =
   struct
 
-    type ('e, 's) t = ('s, ('e Expr.t)) Stmt.t
+    type ('e, 's) t = ('s, ('e Expr.t')) Stmt.t'
     
     ostap (
       expr : !(Expr.parse)[primary];
