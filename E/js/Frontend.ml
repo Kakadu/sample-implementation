2be2768d2b01506bb8103d3c5d2dd05f5ea3f739
dboulytchev <dboulytchev@gmail.com>
2015-03-07 14:29:32 +0000

    Better error reporting

--- E/js/Frontend.ml
+++ E/js/Frontend.ml
@@ -11,12 +11,14 @@ let parse source =
          match Ostap.Msg.loc msg with
          | Ostap.Msg.Locator.Point (line, col) ->
             let decorated = Buffer.create 256 in
-            let l, c, newline, newcol =
-              let l, c = ref 1, ref 1 in
+            let l, c, newline, newcol, mark, marked =
+              let l, c, f = ref 1, ref 1, ref false in
               (fun _ -> !l),
               (fun _ -> !c),
               (fun _ -> incr l; c := 1),
-              (fun _ -> incr c)
+              (fun _ -> incr c),
+              (fun _ -> f := true),
+              (fun _ -> !f)
             in
             String.iter 
               (function 
@@ -25,12 +27,15 @@ let parse source =
                       begin
                         if c () = col then Buffer.add_string decorated "<font color=red>";
                         Buffer.add_string decorated (HTMLView.escape (String.make 1 x));
-                        if c () = col then Buffer.add_string decorated "</font>"
+                        if c () = col then begin
+                          Buffer.add_string decorated "</font>";
+                          mark ()
+                        end
                       end;
                       newcol ()               
-              ) (source ^ "|");            
+              ) source;            
             let string = HTMLView.string (Ostap.Msg.string msg) in
-            let source = View.string (Buffer.contents decorated) in
+            let source = View.string (Buffer.contents decorated ^ if marked () then "" else "<font color=red>|</font>") in
             HTMLView.seq [source; HTMLView.br; string]
          | _ -> default
       in
