333473f027df5bdc6edb44f1a2e2f5fbd2da2b48
dboulytchev <dboulytchev@gmail.com>
2015-03-09 22:27:39 +0000

    Implemented extensible expressions

--- E/base/E.ml
+++ E/base/E.ml
@@ -1,70 +1,31 @@
 open Ostap.Pretty
-
-let w p px x = if px < p then rboxed x else x 
-
-let b l = hboxed  (listBySpaceBreak l)
-let v l = hvboxed (listBySpaceBreak l)
-let c l = cboxed  (vboxed (listByBreak l))
-
 open GT
 
 module Expr =
   struct
 
-    module L = Lexer.Make (struct let keywords = [] end) 
-
     @type 'a t = [  
-        `Var   of string 
-      | `Const of int
       | `Binop of (int -> int -> int) * string * 'a * 'a
     ] with html, show, foldl
-
-    let prio = 
-      let a = [ 
-        "&&", 4; 
-        "==", 5; 
-        "+" , 6;
-        "/" , 7;
-      ] 
-      in
-      function `Binop (_, s, _, _) -> List.assoc s a | _ -> 8
 
     class ['a] eval =
       object (this)
         inherit ['a, int State.t, int, int State.t, int] @t
-        method c_Var   s _ x       = State.get s x
-        method c_Const _ _ n       = n
         method c_Binop s _ f _ x y = f (x.fx s) (y.fx s)
-      end
-
-    class ['a] html' cb =
-      object (this)
-        inherit ['a] @t[html]
-        method attribute (t : 'a t) = cb t
       end
 
     class ['a] vertical =
       object (this)
         inherit ['a] @t[show]
-        method c_Var _ _ x         = Printf.sprintf "x\n%s\n" x
         method c_Binop _ _ _ s x y = Printf.sprintf "*\n%s\n%s%s" s (x.GT.fx ()) (y.GT.fx ())
-        method c_Const _ _ i       = Printf.sprintf "c\n%d\n" i
       end
     
-    let rec html cb e = 
-      HTMLView.li ~attrs:(cb e)
-        (transform(t) (fun _ -> html cb) (new html' cb) () e)
+    let ior  x y = abs x + abs y 
+    let iand x y = abs (x * y) 
+    let b f      = fun x y -> if f x y then 1 else 0 
 
-    let rec vertical e = transform(t) (fun _ -> vertical) (new vertical) () e
-
-    let parse s = 
+    let parse ops primary s = 
       let h = Helpers.highlighting () in
-      let primary p = ostap (
-          l:($) x:!(L.ident)   r:($) {h#register (`Var x) l r}
-        | l:($) i:!(L.literal) r:($) {h#register (`Const i) l r}
-        | l:($) "(" x:p ")"    r:($) {h#register x l r}
-      ) 
-      in
       let rec parse s =  
         let l = List.map 
           (fun (s, t) -> 
@@ -75,15 +36,7 @@ module Expr =
              )
           ) 
         in
-        let ior  x y = abs x + abs y in
-        let iand x y = abs (x * y) in
-        let b f = fun x y -> if f x y then 1 else 0 in
-        Ostap.Util.expr (Helpers.loc h#register) [|
-          `Lefta, l ["&&", iand];
-          `Nona , l ["==", b(=)];
-          `Lefta, l ["+" , ( +  )];
-          `Lefta, l ["/" , (/ )];
-        |]
+        Ostap.Util.expr (Helpers.loc h#register) (Array.map (fun (a, s) -> a, l s) ops)
         (primary parse)
         s
       in
@@ -91,8 +44,63 @@ module Expr =
 
   end
 
+module SimpleExpr =
+  struct
+
+    module L = Lexer.Make (struct let keywords = [] end) 
+
+    @type primary = [`Var of string | `Const of int] with html, show, foldl
+
+    let parse s =
+      let primary p = ostap (
+         x:!(L.ident)   {`Var x}
+        |  i:!(L.literal) {`Const i}
+        |  -"(" p -")"  
+        )
+      in
+      Expr.parse [|
+        `Lefta, ["&&", Expr.iand]; 
+        `Nona , ["==", Expr.b(=)]; 
+        `Lefta, ["+", (+)]; 
+        `Lefta, ["/", (/)]
+      |] 
+      primary s;;
+
+    @type 'a expr = ['a Expr.t | primary] with html, show, foldl
+
+    class ['a] html cb =
+      object (this)
+        inherit ['a] @expr[html]
+      end
+
+    class ['a] vertical =
+      object (this)
+        inherit ['a] @expr[show]
+        inherit ['a] Expr.vertical
+        method c_Var _ _ x = Printf.sprintf "x\n%s\n" x
+        method c_Const _ _ i = Printf.sprintf "c\n%d\n" i
+      end
+
+    class ['a] eval =
+      object (this)
+        inherit ['a] Expr.eval
+        inherit ['a, int State.t, int, int State.t, int] @expr
+        method c_Var s _ x = State.get s x
+        method c_Const _ _ i = i
+      end
+
+    let rec html cb e = 
+      HTMLView.li ~attrs:(cb e)
+        (transform(expr) (fun _ -> html cb) (new html cb) () e)
+
+    let rec vertical e = transform(expr) (fun _ -> vertical) (new vertical) () e      
+
+    let rec eval s e = transform(expr) eval (new eval) s e      
+
+  end
+
 let toplevel source =  
-  match Expr.L.fromString Expr.parse source with
+  match SimpleExpr.L.fromString SimpleExpr.parse source with
   | Checked.Ok (p, h) -> 
       Checked.Ok (
         let interval cb t = 
@@ -105,10 +113,8 @@ let toplevel source =
         object 
           method ast cb =             
             HTMLView.ul ~attrs:"id=\"ast\" class=\"mktree\"" 
-              (*HTMLView.li ~attrs:(interval cb p*) 
-                 (Expr.html (interval cb) p)
-             (* *)
-          method print    = View.string (Expr.vertical p)
+                 (SimpleExpr.html (interval cb) p)             
+          method print    = View.string (SimpleExpr.vertical p)
           method code     = invalid_arg ""
           method run      = invalid_arg ""
           method compile  = invalid_arg ""
