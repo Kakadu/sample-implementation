f111d55785dab1e8892a5150550e9e64b60f4e6c
dboulytchev <dboulytchev@gmail.com>
2015-04-07 19:39:41 +0000

    Expr.v ok

--- coq/Expr.v
+++ coq/Expr.v
@@ -19,12 +19,26 @@ Inductive expr : Type :=
   | And : expr -> expr -> expr
   | Or  : expr -> expr -> expr.
 
+Notation "x '[+]'  y" := (Add x y) (at level 40, left associativity).
+Notation "x '[-]'  y" := (Sub x y) (at level 40, left associativity).
+Notation "x '[*]'  y" := (Mul x y) (at level 41, left associativity).
+Notation "x '[/]'  y" := (Div x y) (at level 41, left associativity).
+Notation "x '[%]'  y" := (Mod x y) (at level 41, left associativity).
+Notation "x '[<=]' y" := (Le  x y) (at level 39, no associativity).
+Notation "x '[<]'  y" := (Lt  x y) (at level 39, no associativity).
+Notation "x '[>=]' y" := (Ge  x y) (at level 39, no associativity).
+Notation "x '[>]'  y" := (Gt  x y) (at level 39, no associativity).
+Notation "x '[==]' y" := (Eq  x y) (at level 39, no associativity).
+Notation "x '[/=]' y" := (Ne  x y) (at level 39, no associativity).
+Notation "x '[&]'  y" := (And x y) (at level 38, left associativity).
+Notation "x '[\/]' y" := (Or  x y) (at level 38, left associativity).
+
 Hint Constructors expr.
 
-Definition zbool (x : Z) : Prop := x = Z.of_nat 1 \/ x = Z.of_nat 0.
+Definition zbool (x : Z) : Prop := x = Z.one \/ x = Z.zero.
   
 Definition zor (x y : Z) : Z :=
-  if Z_le_gt_dec (Z.of_nat 1) (x + y) then Z.of_nat 1 else Z.of_nat 0.
+  if Z_le_gt_dec (Z.of_nat 1) (x + y) then Z.one else Z.zero.
 
 Reserved Notation "[| e |] st => z" (at level 0).
 Notation "st / x => y" := (st_binds Z st x y) (at level 0).
@@ -34,52 +48,62 @@ Inductive bs_eval : expr -> state Z -> Z -> Prop :=
 | bs_Var  : forall (s : state Z) (i : id) (z : Z), s / i => z -> [| Var i |] s => z
 
 | bs_Add  : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> [| Add a b |] s => (za + zb)
+             [| a |] s => za -> [| b |] s => zb -> [| a [+] b |] s => (za + zb)
 | bs_Sub  : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> [| Sub a b |] s => (za - zb)
+             [| a |] s => za -> [| b |] s => zb -> [| a [-] b |] s => (za - zb)
 | bs_Mul  : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> [| Mul a b |] s => (za * zb)
+             [| a |] s => za -> [| b |] s => zb -> [| a [*] b |] s => (za * zb)
 | bs_Div  : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> [| Div a b |] s => (Z.div za zb)
+             [| a |] s => za -> [| b |] s => zb -> [| a [/] b |] s => (Z.div za zb)
 | bs_Mod  : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> [| Mod a b |] s => (Z.modulo za zb)
+             [| a |] s => za -> [| b |] s => zb -> [| a [%] b |] s => (Z.modulo za zb)
 
 | bs_Le_T : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.le za zb -> [| Le a b |] s => (Z.of_nat 1)
+             [| a |] s => za -> [| b |] s => zb -> Z.le za zb -> [| a [<=] b |] s => Z.one
 | bs_Le_F : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.gt za zb -> [| Le a b |] s => (Z.of_nat 0)
+             [| a |] s => za -> [| b |] s => zb -> Z.gt za zb -> [| a [<=] b |] s => Z.zero
 
 | bs_Lt_T : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.lt za zb -> [| Lt a b |] s => (Z.of_nat 1)
+             [| a |] s => za -> [| b |] s => zb -> Z.lt za zb -> [| a [<] b |] s => Z.one
 | bs_Lt_F : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.ge za zb -> [| Lt a b |] s => (Z.of_nat 0)
+             [| a |] s => za -> [| b |] s => zb -> Z.ge za zb -> [| a [<] b |] s => Z.zero
 
 | bs_Ge_T : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.ge za zb -> [| Ge a b |] s => (Z.of_nat 1)
+             [| a |] s => za -> [| b |] s => zb -> Z.ge za zb -> [| a [>=] b |] s => Z.one
 | bs_Ge_F : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.lt za zb -> [| Ge a b |] s => (Z.of_nat 0)
+             [| a |] s => za -> [| b |] s => zb -> Z.lt za zb -> [| a [>=] b |] s => Z.zero
 
 | bs_Gt_T : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.gt za zb -> [| Gt a b |] s => (Z.of_nat 1)
+             [| a |] s => za -> [| b |] s => zb -> Z.gt za zb -> [| a [>] b |] s => Z.one
 | bs_Gt_F : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.le za zb -> [| Gt a b |] s => (Z.of_nat 0)
+             [| a |] s => za -> [| b |] s => zb -> Z.le za zb -> [| a [>] b |] s => Z.zero
 
 | bs_Eq_T : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.eq za zb -> [| Eq a b |] s => (Z.of_nat 1)
+             [| a |] s => za -> [| b |] s => zb -> Z.eq za zb -> [| a [==] b |] s => Z.one
 | bs_Eq_F : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> ~ Z.eq za zb -> [| Eq a b |] s => (Z.of_nat 0)
+             [| a |] s => za -> [| b |] s => zb -> ~ Z.eq za zb -> [| a [==] b |] s => Z.zero
 
 | bs_Ne_T : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> ~ Z.eq za zb -> [| Ne a b |] s => (Z.of_nat 1)
+             [| a |] s => za -> [| b |] s => zb -> ~ Z.eq za zb -> [| a [/=] b |] s => Z.one
 | bs_Ne_F : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> Z.eq za zb -> [| Ne a b |] s => (Z.of_nat 0)
+             [| a |] s => za -> [| b |] s => zb -> Z.eq za zb -> [| a [/=] b |] s => Z.zero
 
 | bs_And  : forall (s : state Z) (a b : expr) (za zb : Z), 
-             [| a |] s => za -> [| b |] s => zb -> zbool za -> zbool zb -> [| And a b |] s => (za * zb)
+             [| a |] s => za -> [| b |] s => zb -> zbool za -> zbool zb -> [| a [&] b |] s => (za * zb)
 
 | bs_Or   : forall (s : state Z) (a b : expr) (za zb : Z), 
-              [| a |] s => za -> [| b |] s => zb -> zbool za -> zbool zb -> [| Or a b |] s => (zor za zb)
+              [| a |] s => za -> [| b |] s => zb -> zbool za -> zbool zb -> [| a [\/] b |] s => (zor za zb)
 where "[| e |] st => z" := (bs_eval e st z). 
 
 Lemma nat_always : forall (n : nat) (s : state Z), [| Nat n |] s => (Z.of_nat n).
 Proof. intros n s. apply bs_Nat. Qed.
+
+Lemma double_and_sum : forall (s : state Z) (e : expr) (z : Z), [| e [*] (Nat 2) |] s => z -> [| e [+] e |] s => z.
+Proof. 
+  intros s e z H. 
+    inversion H. inversion H5. 
+      assert (A: (za * Z.of_nat 2)%Z = (za + za)%Z). simpl. omega.
+    rewrite A. apply bs_Add; assumption.
+Qed.
+
+  
