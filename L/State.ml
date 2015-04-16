9581ba2c1ad8a91ea7f75cbe84aa1b0013943dc8
dboulytchev <dboulytchev@gmail.com>
2015-04-16 11:56:58 +0000

    Brushed up sources

--- L/State.ml
+++
@@ -1,4 +0,0 @@
-type t = string -> int
-
-let empty           = fun x     -> invalid_arg (Printf.sprintf "variable \"%s\" undefined" x)
-let modify s name x = fun name' -> if name = name' then x else s name'
