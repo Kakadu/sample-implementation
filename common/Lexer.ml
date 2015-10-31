2d7940ce3ea70ec72d930ccb4ad2b0736d4d3f83
kakadu <kakadu.hafanana@gmail.com>
2015-10-31 14:11:59 +0400

    Ocamlbuild support

--- common/Lexer.ml
+++ common/Lexer.ml
@@ -25,7 +25,7 @@ module type Sig =
          .. >
        as 'a) ->
       ('a, string, 'b) Ostap.Types.result
- 
+
     val literal :
       (< getLITERAL : ('a, Ostap.Matcher.Token.t, 'b)
                       Ostap.Types.result;
@@ -53,38 +53,38 @@ module Make (K : sig val keywords : string list end) =
 
     let r = Ostap.Matcher.Token.repr
 
-    let is_keyword = 
+    let is_keyword =
       let module S = Set.Make (String) in
       let s = List.fold_left (fun s k -> S.add k s) S.empty K.keywords in
-      (fun i -> S.mem i s)     
+      (fun i -> S.mem i s)
 
     ostap (
       ident  : x:IDENT =>{not (is_keyword (r x))}=> {r x};
-      literal: x:LITERAL {int_of_string (r x)} 
+      literal: x:LITERAL {int_of_string (r x)}
     )
 
-    class t s = 
+    class t s =
       let skip = Ostap.Matcher.Skip.create [
-                   Ostap.Matcher.Skip.whitespaces " \n\t\r"; 
+                   Ostap.Matcher.Skip.whitespaces " \n\t\r";
                    Ostap.Matcher.Skip.nestedComment "(*" "*)";
                    Ostap.Matcher.Skip.lineComment "--"
-                 ] 
+                 ]
       in
 
-      let ident   = Re_str.regexp "[a-zA-Z_]\([a-zA-Z_0-9]\)*\\b" in 
-      let literal = Re_str.regexp "-?[0-9]+" in
+      let ident   = Str.regexp "[a-zA-Z_]\([a-zA-Z_0-9]\)*\\b" in
+      let literal = Str.regexp "-?[0-9]+" in
       object (self)
-        inherit Ostap.Matcher.t s 
+        inherit Ostap.Matcher.t s
         method skip p coord = skip s p coord
         method getIDENT     = self#get "identifier" ident
-        method getLITERAL   = self#get "literal"    literal         
+        method getLITERAL   = self#get "literal"    literal
       end
 
     exception Error of Ostap.Msg.t
 
     let fromString p s =
       try
-        Ostap.Combinators.unwrap (p (new t s)) (fun x -> Checked.Ok x) 
+        Ostap.Combinators.unwrap (p (new t s)) (fun x -> Checked.Ok x)
           (fun (Some err) ->
              let [loc, m :: _] = err#retrieve (`First 1) (`Desc) in
              let m =  match m with `Msg m -> m | `Comment (s, _) -> Ostap.Msg.make s [||] loc in
@@ -93,4 +93,3 @@ module Make (K : sig val keywords : string list end) =
       with Error m -> Checked.Fail [m]
 
   end
-
